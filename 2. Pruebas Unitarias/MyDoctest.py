725966d25e1865deea42a95581456261bcb42d72
Dany Zambrano <danyzambranososa@gmail.com>
2023-09-05 23:16:38 -0400

    doctest

---
+++ 2. Pruebas Unitarias/MyDoctest.py
@@ -0,0 +1,26 @@
+
+
+# doctest
+def sumatoria(valorA, valorB, valorC):
+
+    '''
+        Funcion para el calculo de la suma total de valores
+
+        Argumentos: valores enteros
+
+        Ejemplo:
+            sumatoria(8, 2, 6)
+
+        >>> sumatoria(2, 2, 1)
+        5
+
+        >>> sumatoria(1, 2, 3)
+        6
+
+        >>> sumatoria(1, 1, 1)
+        3
+
+    '''
+    suma = valorA + valorB + valorC
+    print(suma)
+
