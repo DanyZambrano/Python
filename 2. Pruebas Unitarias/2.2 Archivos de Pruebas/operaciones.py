91c0b03298aa77bdbeac02b8d35543216077fa7d
Dany Zambrano <danyzambranososa@gmail.com>
2023-09-06 08:25:59 -0400

    archivo de prueba

---
+++ 2. Pruebas Unitarias/2.2 Archivos de Pruebas/operaciones.py
@@ -0,0 +1,16 @@
+
+
+
+def sumatoria(valorA, valorB, valorC):
+
+    '''
+        Funcion para el calculo de la suma total de valores
+
+        Argumentos: valores enteros
+
+        Ejemplo:
+            sumatoria(8, 2, 6)
+    '''
+    suma = valorA + valorB + valorC
+    print(suma)
+
